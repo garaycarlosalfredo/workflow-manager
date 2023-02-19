feca039c163a2d71f110d5e24f06659d65603b3e
carlos <garaycarlosalfredo@gmail.com>
2023-02-19 10:15:54 -0300

    feat: add back urls

--- lambda-backend/src/service/axios.ts
+++ lambda-backend/src/service/axios.ts
@@ -1,8 +1,8 @@
 const axios = require("axios");
 //const { SLACK_URL_TEST } = process.env;
-const url = "http://localhost:4000";
+//const url = "http://localhost:4000";
 
-const postRequest = async (data) => {
+const postRequest = async (url: string,data: Object) => {
   const res = await axios.post(url, data);
   console.log("post request success");
   return res;
--- lambda-backend/src/user/user.ts
+++ lambda-backend/src/user/user.ts
@@ -1,7 +1,8 @@
 const { postRequest } = require("../service/axios.ts");
+const {URL_USERS} = process.env
 
 const userRequest = async (data) => {
-  const res = await postRequest({});
+  const res = await postRequest(URL_USERS, {});
   console.log(`${res} ${typeof res}`, res.data.message);
   return res.data.message;
 };
