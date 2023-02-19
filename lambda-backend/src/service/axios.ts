c14b32345c2c3e6343e83c3b6fc3594afd45e017
carlos <garaycarlosalfredo@gmail.com>
2023-02-19 12:27:47 -0300

    feat: remove uneccesary lines

--- lambda-backend/src/service/axios.ts
+++ lambda-backend/src/service/axios.ts
@@ -2,7 +2,7 @@ const axios = require("axios");
 //const { SLACK_URL_TEST } = process.env;
 //const url = "http://localhost:4000";
 
-const postRequest = async (url: string,data: Object) => {
+const postRequest = async (url,data) => {
   const res = await axios.post(url, data);
   console.log("post request success");
   return res;
